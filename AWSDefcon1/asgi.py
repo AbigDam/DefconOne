476e1dc9a0ca96d6ba19602c9d9282e5f7812db3
ABigDam <randomdams@gmail.com>
2025-12-26 16:51:21 -0500

    Fix Asgi

--- AWSDefcon1/asgi.py
+++ AWSDefcon1/asgi.py
@@ -1,7 +1,18 @@
 import os
-from channels.routing import get_default_application
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
 import django
+import AWSDefcon1App.routing   # ← CHANGE to your app name
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "AWSDefcon1.settings")
 django.setup()
-application = get_default_application()
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            AWSDefcon1App.routing.websocket_urlpatterns
+        )
+    ),
+})
