6e035fe2ab3be65c169198a13516b440e2a169ec
ABigDam <randomdams@gmail.com>
2025-12-26 14:37:26 -0500

    Fixed redis again

--- AWSDefcon1/asgi.py
+++ AWSDefcon1/asgi.py
@@ -2,6 +2,6 @@ import os
 from channels.routing import get_default_application
 import django
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "your_project.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "AWSDefcon1.settings")
 django.setup()
 application = get_default_application()
